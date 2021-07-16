e7109c5039f681c769dd4ef5ff88e7a25453132c
Myo Myat Han <wildin024@gmail.com>
2021-07-16 08:48:46 +0630

    start auth

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    $id = $_POST['id'];
+    $password = $_POST['password'];
+    
+    if($id == "admin@gmail.com" && $password == "admin") {
+    $_SESSION['auth'] = true;
+    $_SESSION['id'] = "Admin";
+    }
+    header("location: index.php");
+?>
